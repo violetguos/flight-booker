3b472035c454f5544364e50c94b944b45f817529
Y. Violet Guo <violetguos@users.noreply.github.com>
2021-01-27 19:59:49 -0400

    Hacked a stripe credit card together with default values

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,11 +1,10 @@
 class ChargesController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     # Amount in cents
     @amount = 500
-
+    Stripe.api_key = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
     customer = Stripe::Customer.create({
                                          email: params[:stripeEmail],
                                          source: params[:stripeToken]
@@ -17,10 +16,9 @@ class ChargesController < ApplicationController
                                      description: 'Rails Stripe customer',
                                      currency: 'usd'
                                    })
+    flash[:notice] = 'Fictional ticket booked!' if charge
   rescue Stripe::CardError => e
     flash[:error] = e.message
-    redirect_to new_charge_path
+    redirect_to root_path
   end
-
-
 end
